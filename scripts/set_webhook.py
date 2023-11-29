a37b429a474070da35c5c50c4350bc407cccc85b
daridev <darideveloper@gmail.com>
2023-11-29 13:07:55 -0600

    feat (scripts/set_webhook): multiple bots

--- scripts/set_webhook.py
+++ scripts/set_webhook.py
@@ -1,16 +1,32 @@
 import os
+import csv
 import requests
 from dotenv import load_dotenv
 load_dotenv()
 
 # Get settings
 WEBHOOK = os.getenv('WEBHOOK')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-# Generate set webhook bot url
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK}"
+# generate csv path
+current_folder = os.path.dirname(__file__)
+parent_folder = os.path.dirname(current_folder)
+csv_path = os.path.join(parent_folder, 'bots_tokens.csv')
 
-# Send request
-res = requests.get(url)
-res.raise_for_status()
-print(res.json())
+# read csv file
+with open(csv_path, 'r') as csv_file:
+    reader = list(csv.reader(csv_file))
+    for row in reader[1:]:
+        bot_name, bot_token = row
+
+        print(f"setting webhook for {bot_name}...")
+
+        # Generate set webhook bot url
+        url = f"https://api.telegram.org/bot{bot_token}" \
+            f"/setWebhook?url={WEBHOOK}"
+
+        # Send request
+        res = requests.get(url)
+        res.raise_for_status()
+
+        # Print response
+        print(f"\tStatus code: {res.status_code}")
